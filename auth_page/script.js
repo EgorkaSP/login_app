39ab77326eb776be85652f4865de09e5439d271b
Egor <fomin0596@gmail.com>
2022-10-20 12:43:56 +0300

    initial commit

---
+++ auth_page/script.js
@@ -0,0 +1,18 @@
+const usernameValue = document.getElementById('input-field');
+const passwordValue = document.getElementById('password-field');
+
+const submitForm = (event) => {
+    event.preventDefault(event);
+   
+    if (usernameValue.value.trim() === '' ||
+        passwordValue.value.trim() === '') {
+        alert('Введите логин и пароль!');
+    } else {
+        let validationData = `${usernameValue.value} - ${passwordValue.value}`;
+        localStorage.setItem('token', validationData);
+        // window.location.href = 
+        window.open('./index2.html');
+        usernameValue.value = '';
+        passwordValue.value = '';
+    } 
+}
